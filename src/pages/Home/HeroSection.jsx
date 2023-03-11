ee16841593faccc9c409bf2db05e98811bb54a38
Rakibul Islam <rakib245875@gmail.com>
2023-03-12 00:43:15 +0600

    add hero animation

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -20,19 +20,29 @@ export default function HeroSection() {
     tl.addLabel("frame1")
       .from(
         ".supplier-logo",
-        { autoAlpha: 0, x: "100%", stagger: 0.2 },
+        { autoAlpha: 0, x: "200%", stagger: 0.2 },
         "frame1+=1"
       )
-      .from(".heading-1", { autoAlpha: 0, x: "-50%" }, "frame1+=2")
-      .to(".frame-1", { autoAlpha: 0, x: "50%" }, "frame+=3");
-    tl.addLabel("frame2", "frame1+=5.5")
+      .from(".heading-1-1", { autoAlpha: 0, y: "-50%" }, "frame1+=2")
+      .to(".heading-1-1", { autoAlpha: 0, y: "50%" }, "frame1+=4")
+      .from(".heading-1-2", { autoAlpha: 0, y: "-50%" }, "frame1+=4.2")
+      .to(".heading-1-2", { autoAlpha: 0, y: "50%" }, "frame1+=6")
+      .from(".heading-1-3", { autoAlpha: 0, y: "-50%" }, "frame1+=6.2")
+      .to(".heading-1-3", { autoAlpha: 0, y: "50%" }, "frame1+=8")
+      .from(".heading-1-4", { autoAlpha: 0, y: "-50%" }, "frame1+=8.2")
+      .to(".heading-1-4", { autoAlpha: 0, y: "50%" }, "frame1+=10")
+      .from(".heading-1-5", { autoAlpha: 0, y: "-50%" }, "frame1+=10.2")
+      // .to(".heading-1-5", { autoAlpha: 0, y: "50%" }, "frame1+=12")
+      .to(".frame-1", { autoAlpha: 0, x: "50%" }, "frame1+=14");
+    tl.addLabel("frame2", "frame1+=14.5")
       .from(".headline1", { autoAlpha: 0, x: "-100%" }, "frame2")
-      .from(".vernon-printer", { autoAlpha: 0, x: "100%" }, "frame2+=.5")
-      .to(
-        ".headline1 , .vernon-printer",
-        { autoAlpha: 0, x: "100%" },
-        "frame2+=3"
-      );
+      .from(".vernon-printer", { autoAlpha: 0, x: "-200%" }, "frame2+=.5")
+      .from(
+        ".vernon-printer-info",
+        { autoAlpha: 0, y: "-50%", stagger: 1 },
+        "frame2+=1"
+      )
+      .to(".frame-2", { autoAlpha: 0, x: "100%" }, "frame2+=6");
     // .to(".vernon-printer", { autoAlpha: 0, x: "-100%" }, "frame2+=3");
     return () => {
       // tl.pause();
@@ -42,41 +52,71 @@ export default function HeroSection() {
     tl.play();
   }, []);
   return (
-    <div className="relative  text-primary min-h-[60vh] w-screen overflow-hidden">
+    <div className="relative text-neutral h-[450px] w-screen overflow-hidden">
       <div className="frame-1 w-full h-full absolute">
-        <h2 className="heading-1 text-xl md:text-2xl lg:text-5xl uppercase italic font-extrabold text-center tracking-wide absolute w-full top-[80%] invisible">
-          Complete bach coding solution <br /> at one place!
-        </h2>
         <img
-          className="ps-logo absolute w-[150px] lg:w-[200px] h-auto top-[10%] left-[50%] translate-x-[-50%]"
+          className="ps-logo absolute w-[100px] lg:w-[100px] h-auto top-[5%] left-[50%] translate-x-[-50%]"
           src={psLogo}
           alt="Vernon bach ink jet printer"
         />
         <img
-          className="vernon-logo supplier-logo absolute w-[250px] h-auto top-[60%] left-[20%] translate-x-[-50%]"
+          className="vernon-logo supplier-logo absolute w-[250px] h-auto top-[40%] left-[20%] translate-x-[-50%]"
           src={vernonLogo}
           alt="Vernon bach ink jet printer"
         />
         <img
-          className="btmark-logo supplier-logo absolute w-[200px] h-auto top-[58%] left-[50%] translate-x-[-50%]"
+          className="btmark-logo supplier-logo absolute w-[200px] h-auto top-[38%] left-[50%] translate-x-[-50%]"
           src={btmarkLogo}
           alt="Vernon bach ink jet printer"
         />
         <img
-          className="mycoding-logo supplier-logo absolute w-[100px] h-auto top-[53%] left-[75%] translate-x-[-50%]"
+          className="mycoding-logo supplier-logo absolute w-[100px] h-auto top-[33%] left-[75%] translate-x-[-50%]"
           src={mycodingLogo}
           alt="Vernon bach ink jet printer"
         />
+        <h2 className="heading-1-1 text-xl md:text-2xl lg:text-5xl uppercase italic font-bold text-center tracking-wide absolute w-full top-[60%] invisible">
+          CIJ Ink Jet Printer
+        </h2>
+        <h2 className="heading-1-2 text-xl md:text-2xl lg:text-5xl uppercase italic font-bold text-center tracking-wide absolute w-full top-[60%] invisible">
+          Thermal Ink Jet Printer
+        </h2>
+        <h2 className="heading-1-3 text-xl md:text-2xl lg:text-5xl uppercase italic font-bold text-center tracking-wide absolute w-full top-[60%] invisible">
+          Hand-held Printers
+        </h2>
+        <h2 className="heading-1-4 text-xl md:text-2xl lg:text-5xl uppercase italic font-bold text-center tracking-wide absolute w-full top-[60%] invisible">
+          Conveyors & rewinders
+        </h2>
+        <h2 className="heading-1-5  text-primary text-xl md:text-2xl lg:text-5xl uppercase italic font-bold text-center tracking-wide absolute w-full top-[60%] invisible">
+          Complete bach coding solution
+        </h2>
       </div>
 
-      <h2 className="headline1 text-xl md:text-2xl lg:text-4xl uppercase italic font-bold text-center tracking-wide absolute w-full top-[20%] invisible">
-        Sole distributor of <br /> Vernon Bach Hubei Tech. Ltd
-      </h2>
-      <img
-        className="vernon-printer absolute w-[200px] h-auto top-[50%] left-[50%] translate-x-[-50%]"
-        src={vernonBachPrinter}
-        alt="Vernon bach ink jet printer"
-      />
+      <div className="frame-2 absolute w-full h-full">
+        <h2 className="headline1 text-xl md:text-2xl lg:text-4xl uppercase italic font-bold text-center tracking-wide absolute w-full top-[20%] invisible">
+          <span className="text-neutral text-lg md:text-xl lg:text-2xl">
+            {" "}
+            Sole distributor of{" "}
+          </span>
+          <br /> Vernon Bach Hubei Tech. Ltd
+        </h2>
+        <img
+          className="vernon-printer absolute w-[250px] h-auto top-[50%] left-[35%] translate-x-[-50%]"
+          src={vernonBachPrinter}
+          alt="Vernon bach ink jet printer"
+        />
+        <h3 className="vernon-printer-info text-lg md:text-xl lg:text-2xl capitalize tracking-wide absolute w-full top-[60%] left-[50%] invisible">
+          Technology from GERMANY
+        </h3>
+        <h3 className="vernon-printer-info text-lg md:text-xl lg:text-2xl capitalize tracking-wide absolute w-full top-[68%] left-[50%] invisible">
+          3 years warranty
+        </h3>
+        <h3 className="vernon-printer-info text-lg md:text-xl lg:text-2xl capitalize tracking-wide absolute w-full top-[76%] left-[50%] invisible">
+          Reliable and trouble-free
+        </h3>
+        <h3 className="vernon-printer-info text-lg md:text-xl lg:text-2xl capitalize tracking-wide absolute w-full top-[84%] left-[50%] invisible">
+          Hi-speed application
+        </h3>
+      </div>
     </div>
   );
 }
